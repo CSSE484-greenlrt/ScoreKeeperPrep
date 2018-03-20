fe3633d781fd1edae945c27c744c7dbd016d564c
Ryan Greenlee <Ryan@kimberlys-mbp.wlan.rose-hulman.edu>
2018-03-20 12:05:07 -0400

    Initial Commit

---
+++ ScoreKeeperPrep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoreKeeperPrep
+//
+//  Created by Ryan Greenlee on 3/20/18.
+//  Copyright © 2018 Ryan Greenlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScoreKeeperPrep/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScoreKeeperPrep
+//
+//  Created by Ryan Greenlee on 3/20/18.
+//  Copyright © 2018 Ryan Greenlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
